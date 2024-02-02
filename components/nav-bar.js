95bba97e3c0c67c4c168d4dba3c62b2747cbd34f
Blake Wilson <wilso692@purdue.edu>
2024-02-01 21:36:11 -0800

    Added research

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -10,22 +10,22 @@ export default function NavBar({children }) {
       <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <span className="font-semibold text-xl tracking-tight">
-            <a href="/">
+            <Link href="/">
               Blake Wilson
-            </a>
+            </Link>
           </span>
         </div>
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
-            <a href="/research" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+            <Link href="/research" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
               Research
-            </a>
-            <a href="/projects" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+            </Link>
+            <Link href="/projects" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
               Projects
-            </a>
-            <a href="/cv" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+            </Link>
+            <Link href="/cv" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
               CV
-            </a>
+            </Link>
           </div>
         </div>
       </nav>
---
+++ out/_next/static/chunks/pages/resume-556b2800450c924e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[647],{1429:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/resume",function(){return u(9557)}])}},function(n){n.O(0,[247,557,888,774,179],function(){return n(n.s=1429)}),_N_E=n.O()}]);
